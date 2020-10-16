b4ae3f92091b63530492a67a4cffac687d8a4f12
AsherFor <asherforman@stu.postoakschool.org>
2020-10-16 16:05:00 -0700

    Commit

--- Battleship.py
+++ Battleship.py
@@ -9,13 +9,15 @@ def welcome():
 
 
 def player1_board():
-# Player 1 board
+# Draw player 1 board
 
 def player2_board():
-# Player 2 board
+# Draw player 2 board
 
-def place_pieces():
+def player_1_place_ships():
 # Player 1 is going to place their ships on their board
+
+def player_2_place_ships():
 # Player 2 is going to place their ships on their board
 
 
@@ -25,21 +27,37 @@ def player_1_turn():
 def player_2_turn():
 # Player 2 picks a row and a column on Player 1 board to try and sink a ship
 
+def shipsunk_player1():
+# Check to see if a ship was sunk by Player 1
+
+def shipsunk_player2():
+# Check to see if a ship was sunk by Player 2
 
 def didIWin():
-# Check to see if a ship was sunk
-# Check to see if anyone won
+# Check to see if anyone sunk all of the ships
 
 
+
+def ship_placement():
+    # This will let the players choose where they want to place their ships
+    # Loop however many times to place the pieces
+    # player1_board()
+    # player_1_place_ships()
+    # player2_board()
+    # player_2_place_ships()
 
 
 def game_play():
-# Player 1 places their ships
-# Player 2 places their ships
-# Player 1 turn to try and sink a ship
-# Check to see if a ship was sunk or if anyone won
-# Player 2 turn to try and sink a ship
-# Check to see if a ship was sunk or if anyone won
+# This will be the gameplay. The players will take turns trying to sink the other person's ships
+# The loops necessary
+# player1_board()
+# player_1_turn()
+# shipsunk_player1()
+# didIWin()
+# player2_board()
+# player_2_turn()
+# shipsunk_player2()
+# didIWin()
 
 
 
