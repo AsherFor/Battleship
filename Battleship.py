9cf3279ee3b09284f5b8db6ce18e123a9e90e4ba
AsherFor <asherforman@stu.postoakschool.org>
2020-10-16 08:40:13 -0700

    Initial commit

---
+++ Battleship.py
@@ -0,0 +1,45 @@
+'''
+Asher Forman
+Battleship
+10/16/20
+'''
+
+def Welcome():
+# Greeting
+
+
+def player1_board():
+# Draw the board
+
+def player2_board():
+# Draw the board
+
+def place_pieces():
+# Player 1 is going to place their pieces on their board
+# Player 2 is going to place their pieces on their board
+
+
+def player_1_turn():
+# Player 1 picks a row and a column to try and sink a ship
+
+def player_2_turn():
+# Player 2 picks a row and a column to try and sink a ship
+
+
+def didIWin():
+# Check to see if a ship was sunk
+# Check to see if anyone won
+
+
+
+
+def game_player():
+# Player 1 places pieces
+# Player 2 places pieces
+# Player 1 turn to try and sink a ship
+# Check to see if a ship was sunk or if anyone won
+# Player 2 turn to try and sink a ship
+# Check to see if a ship was sunk or if anyone won
+
+
+
