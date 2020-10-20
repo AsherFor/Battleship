1c4f5870ac59c06bb358072bb2cd8d06abd9e8d9
AsherFor <71336882+AsherFor@users.noreply.github.com>
2020-10-19 22:33:10 -0500

    Update Battleship.py

--- Battleship.py
+++ Battleship.py
@@ -5,36 +5,36 @@ Battleship
 '''
 
 def welcome():
-# Greeting the players
+    # Greeting the players
 
 
 def player1_board():
-# Draw player 1 board
+    # Draw player 1 board
 
 def player2_board():
-# Draw player 2 board
+    #Draw player 2 board
 
 def player_1_place_ships():
-# Player 1 is going to place their ships on their board
+    # Player 1 is going to place their ships on their board
 
 def player_2_place_ships():
-# Player 2 is going to place their ships on their board
+    # Player 2 is going to place their ships on their board
 
 
 def player_1_turn():
-# Player 1 picks a row and a column on Player 2 board to try and sink a ship
+    # Player 1 picks a row and a column on Player 2 board to try and sink a ship
 
 def player_2_turn():
-# Player 2 picks a row and a column on Player 1 board to try and sink a ship
+    # Player 2 picks a row and a column on Player 1 board to try and sink a ship
 
 def shipsunk_player1():
-# Check to see if a ship was sunk by Player 1
+    # Check to see if a ship was sunk by Player 1
 
 def shipsunk_player2():
-# Check to see if a ship was sunk by Player 2
+    # Check to see if a ship was sunk by Player 2
 
 def didIWin():
-# Check to see if anyone sunk all of the ships
+    # Check to see if anyone sunk all of the ships
 
 
 
@@ -48,16 +48,14 @@ def ship_placement():
 
 
 def game_play():
-# This will be the gameplay. The players will take turns trying to sink the other person's ships
-# Add loops
-# player1_board()
-# player_1_turn()
-# shipsunk_player1()
-# didIWin()
-# player2_board()
-# player_2_turn()
-# shipsunk_player2()
-# didIWin()
-
-
+    # This will be the gameplay. The players will take turns trying to sink the other person's ships
+    # Add loops
+    # player1_board()
+    # player_1_turn()
+    # shipsunk_player1()
+    # didIWin()
+    # player2_board()
+    # player_2_turn()
+    # shipsunk_player2()
+    # didIWin()
 
